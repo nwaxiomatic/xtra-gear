52a3ce613c0d2a8fbd3fd8b77b3702010e26a708
mac <nicholasdwoodward@gmail.com>
2017-12-06 13:02:26 -0500

    12_6_17 fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,94 +1,31 @@
-"use strict";
+$(document).ready(function() {
+    $(".shop").hover(function(){
+        $("body").css("background-color", "#ffcc00");
+          $(this).css("color", "black");
+        },
+        function(){
+          $("body").css("background-color", "black");
+          $(this).css("color", "#ffcc00");
+        }
+    );
 
-function resizeVideo(){
-    var h = $(document).height();
-    var w = $(document).width();
-    var centerBox = $('.center-box');
-    var size = '70';
-    if(h > w){
-        centerBox.css({
-            'width' : size + 'vw',
-            'height' : size + 'vw',
-        })
+    $('#shop-button').click(function(){
+      window.location = "http://shop.xtragear.services/";
+    });
+
+    var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+
+    if (isMobile){
+      $('.container').css({ 'height': window.innerHeight });
     }
-    else {
-        centerBox.css({
-            'width' : size + 'vh',
-            'height' : size + 'vh',
-        })
-    }
-}
 
-$(window).resize(resizeVideo);
-$(window).resize();
-
-var gl = twgl.getWebGLContext(document.getElementById("c"));
-var programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
-
-var gl2 = twgl.getWebGLContext(document.getElementById("c2"));
-var programInfo2 = twgl.createProgramInfo(gl2, ["vs", "fs2"]);
-var textures2 = null;
-var textures = twgl.createTextures(gl, {
-	face: { src: "static/img/xtra.png" },
-  irridescent: { src: "static/img/holo2.jpg" },
-  cloud1: { src: "static/img/cloud.jpg" },
-  cloud2: { src: "static/img/cloud2.jpg" },
-  cloud3: { src: "static/img/cloud3.jpg" },
-}, function(){
-    textures2 = twgl.createTextures(gl2, {
-      tex: { src: "static/img/holo2.jpg" },
-      irridescent: { src: "static/img/holo1.jpg" },
-      cloud1: { src: "static/img/cloud.jpg" },
-    }, function(){
-      $('.container').addClass('shown');
-      requestAnimationFrame(render);
-  });
 });
 
-var arrays = {
-  position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
-};
-var bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
-var bufferInfo2 = twgl.createBufferInfoFromArrays(gl2, arrays);
-
-function render(time) {
-  twgl.resizeCanvasToDisplaySize(gl.canvas);
-  twgl.resizeCanvasToDisplaySize(gl2.canvas);
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  gl2.viewport(0, 0, gl2.canvas.width, gl2.canvas.height);
-
-  var shadowAmt = 10 * (1 + Math.sin(time / 1000.0) ) / 2.0 + 4.0;
-  $('.breathe').css({
-    'box-shadow' : "0px 0px " + shadowAmt + "px " + shadowAmt + "px #000"
-  });
-
-  var uniforms = {
-    time: time * 0.001,
-    resolution: [gl.canvas.width, gl.canvas.height],
-    tex: textures.face,
-    irridescent: textures.irridescent,
-    phase_tex_r: textures.cloud1,
-    phase_tex_g: textures.cloud2,
-    phase_tex_b: textures.cloud3,
-  };
-
-  var uniforms2 = {
-    time: time * 0.001,
-    resolution: [gl2.canvas.width, gl2.canvas.height],
-    tex: textures2.tex,
-    irridescent: textures2.irridescent,
-    phase_tex_r: textures2.cloud1,
-  };
-
-  gl.useProgram(programInfo.program);
-  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-  twgl.setUniforms(programInfo, uniforms);
-  twgl.drawBufferInfo(gl, bufferInfo);
-
-  gl2.useProgram(programInfo2.program);
-  twgl.setBuffersAndAttributes(gl2, programInfo2, bufferInfo2);
-  twgl.setUniforms(programInfo2, uniforms2);
-  twgl.drawBufferInfo(gl2, bufferInfo2);
-
-  requestAnimationFrame(render);
-}
+$(window).on("load", function() {
+  setTimeout(function(){
+      $('.container').addClass('shown');
+    }, 100);
+});
